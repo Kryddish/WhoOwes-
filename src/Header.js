6bf14d8a120503ba618843e037d2c973680793a0
Memlisen <emmelie.sundell@hotmail.com>
2017-06-13 18:32:40 +0200

    Startpage design 1

--- src/Header.js
+++ src/Header.js
@@ -6,9 +6,50 @@ class Header extends Component {
 
   render() {
     return (
+
     <div>
+     	<div className="hero">
+     	<p className="logo">Who owes?</p>
      	
+	     <div className="intro">
+	     	<em><p>Tired of remeber depths?<br />
+			Let the app do it for you!</p></em>
+		</div>
+
+		<p className="register">Register with your phonenumer:</p>
+	
+    <form className="contact-form">
+     
+         <div className="contact-item">
+          <label for="name">Full name:</label>
+          <input type="text" />
+        </div>
+
+        <div className="contact-item">
+          <label for="email">Email:</label>
+          <input type="text" />
+        </div>
+
+        <div className="contact-item">
+          <label for="phone">Phone:</label>
+          <input type="text"  />
+        </div>
+
+        <div className="contact-item">
+          <label for="address">Password:</label>
+          <input type="text" />
+        </div>
+        <div className="contact-item">
+          <label for="address"></label>
+          <input type="submit" value="Register"/>
+        </div>
+
+        <em><p className="already">Already have an account? Click here to login.</p></em>
+
+    </form>
     </div>
+    </div>
+
     )
   }
 }
