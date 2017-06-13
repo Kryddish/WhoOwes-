06969d64ff557a872c9e5a40602a062483c6d996
Memlisen <emmelie.sundell@hotmail.com>
2017-06-13 13:01:34 +0200

    Who owes?

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './styles/App.css'
+import Header from './Header'
+import Menu from './Menu'
+
+class App extends Component {
+  render() {
+    return (
+
+      <div className="App">
+
+      <p>test</p>
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './styles/App.css'
+import './styles/Header.css'
+
+class Header extends Component {
+
+  render() {
+    return (
+    <div>
+     	
+    </div>
+    )
+  }
+}
+
+export default Header
+
+// logo: https://thumbs.dreamstime.com/t/group-parrots-isolated-white-44429802.jpg
